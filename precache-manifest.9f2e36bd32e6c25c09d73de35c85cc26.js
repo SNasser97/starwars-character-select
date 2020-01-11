67cc6feb2d6c3147a86430bc2f1c95415ca96b76
Nasser <37514151+SNasser97@users.noreply.github.com>
2020-01-11 01:23:38 +0000

    Updates

---
+++ precache-manifest.9f2e36bd32e6c25c09d73de35c85cc26.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4ee8e9dced3bf830f5c6c65470986d9",
+    "url": "/starwars-character-select/index.html"
+  },
+  {
+    "revision": "2cd3e613d1e0814e3a91",
+    "url": "/starwars-character-select/static/css/main.80423205.chunk.css"
+  },
+  {
+    "revision": "68451d58b5ab67fc0d30",
+    "url": "/starwars-character-select/static/js/2.2a5ba0df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/starwars-character-select/static/js/2.2a5ba0df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cd3e613d1e0814e3a91",
+    "url": "/starwars-character-select/static/js/main.77aacc1b.chunk.js"
+  },
+  {
+    "revision": "5e99002d1fbffdf04ca7",
+    "url": "/starwars-character-select/static/js/runtime-main.e815f19f.js"
+  },
+  {
+    "revision": "ac91d5f1c08a4b0a46da02718030f147",
+    "url": "/starwars-character-select/static/media/DeathStar.ac91d5f1.woff"
+  },
+  {
+    "revision": "e8e3c5d204674ba5ce904de85cc0b858",
+    "url": "/starwars-character-select/static/media/DeathStar.e8e3c5d2.ttf"
+  }
+]);
